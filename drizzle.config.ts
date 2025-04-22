219b4c5d742bf3738d6c1cffd941d62d9a41bc19
João Branco <joaombranco92@gmail.com>
2025-04-22 13:50:55 +0100

    fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "drizzle-kit";
 
+const IS_IN_TESTING_ENV = process.env.ENVIRONMENT === "testing";
+
 export default defineConfig({
-  schema: "./src/db/schema.ts",
-  out: "./migrations",
-  dialect: "turso",
+  schema: IS_IN_TESTING_ENV ? undefined : "./src/db/schema.ts",
+  out: IS_IN_TESTING_ENV ? undefined : "./migrations",
+  dialect: IS_IN_TESTING_ENV ? "sqlite" : "turso",
   dbCredentials: {
     url: process.env.TURSO_DB_CONNECTION_URL!,
-    authToken: process.env.ENVIRONMENT === "testing" ? undefined : process.env.TURSO_DB_TOKEN
+    authToken: IS_IN_TESTING_ENV ? undefined : process.env.TURSO_DB_TOKEN
   },
   verbose: true,
-  strict: true
+  strict: !IS_IN_TESTING_ENV
 });
